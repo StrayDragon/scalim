fa00b187bcb64d9d3a750d5993ba7ce26759ffdd
straydragon <straydragonl@foxmail.com>
2026-04-05 17:49:42 +0800

    chore(notebooks): add relative-module loader demo

---
+++ notebooks/marimo/demo_big_data_report/chapters_of_yaml_dsl/declared_yaml_dsl/relative_module_demo/registry.py
@@ -0,0 +1,10 @@
+def load_orders():
+    """Demo loader for relative-module go-to-definition.
+
+    Note:
+      - This is a notebooks fixture module.
+      - The YAML DSL LSP resolves definitions statically via filesystem + AST.
+      - This function is not executed by the LSP server.
+    """
+    return []
+
